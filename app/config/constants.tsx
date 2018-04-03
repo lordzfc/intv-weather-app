6362991250e778a72f35fd6f61fa1fc309a70bff
Pawel Galwa <pawelgalwa@Pawels-MacBook-Pro.local>
2018-04-03 22:01:22 +0200

    Created Constants

---
+++ app/config/constants.tsx
@@ -0,0 +1,4 @@
+export const WEATHER_API_KEY  = "fdd8a1dd8b40a3e7344c0665a7e2598e";
+export const FETCH_CITIES_URL = 'https://api.myjson.com/bins/1ah40j';
+export const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+export const POST_URL = 'http://myjson.com/api';
