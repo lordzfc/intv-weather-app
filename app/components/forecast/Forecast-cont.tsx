332f259e7ad556ac8ce5010c6fa9608c184d56be
Pawel Galwa <pawelgalwa@Pawels-MacBook-Pro.local>
2018-04-04 07:54:36 +0200

    improved component's logic

--- app/components/forecast/Forecast-cont.tsx
+++ app/components/forecast/Forecast-cont.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import {observer} from 'mobx-react';
 import {WeatherFull, WeatherPeriod} from '../../store/Store.d';
+import {ForecastTile} from './Forecast-tile';
+import * as styles from './Forecast.css';
 
 export interface ForecastContProps { weather: WeatherFull }
 
 export class ForecastCont extends React.Component<ForecastContProps, {}> {
-    render() {
-        return <div>Test</div>
+  render() {
+    let weather; 
+    if(this.props.weather) {
+      weather = (
+        <React.Fragment>
+          <h3 className={styles.componentsTitle}> Forecast </h3>
+          {this.props.weather.list.map(
+            (e)=><ForecastTile key={e.dt} weatherPeriod={e}/>
+          )}
+        </React.Fragment>
+      )
     }
+    return <div>
+      {weather}
+    </div>
+  }
 }
--- app/components/forecast/Forecast-tile.tsx
+++ app/components/forecast/Forecast-tile.tsx
@@ -1,3 +1,11 @@
 import * as React from "react";
 import {observer} from 'mobx-react';
 import {WeatherPeriod} from '../../store/Store.d';
+
+export interface ForecastTileProps { weatherPeriod: WeatherPeriod }
+
+export class ForecastTile extends React.Component<ForecastTileProps, {}> {
+    render() {
+        return <div>Test</div>
+    }
+}
